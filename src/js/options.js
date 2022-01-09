02e9718a5d924fcea06a9ecb9d4adc7f08413f99
jionjion <jionjion@aliyun.com>
2022-01-09 21:46:06 +0800

    - 设置页面编写

--- src/js/options.js
+++ src/js/options.js
@@ -1,22 +1,51 @@
+// noinspection JSUnresolvedVariable
+
 /**
  *  设置界面.用户自定义配置.
  *
  *  @author Jion
  */
 
+
+/*
+ *	各种Dom元素对象
+ */
+let $queryForm = document.querySelector("#app_key");
+let $queryForm = document.querySelector("#app_secret_key");
+let $queryForm = document.querySelector("#enable_draw_translation");
+let $queryForm = document.querySelector("#draw_translation_secondary_key_by_none");
+let $queryForm = document.querySelector("#draw_translation_default_voice_by_uk");
+
+
+/*
+ * 配置对象
+ */
+const config = {
+    'appKey':'1666f504b10dfd2c',
+    'appSecretKey':'gKzqWsuGtU3efU4qUlWoR2knV1Q4LST',
+    'enableDrawTranslation': 'Y',
+    'drawTranslationSecondaryKey':'None',
+    'drawTranslationDefaultVoice':'uk',
+};
+
+
+
+
+// 页面加载后执行
 (() => {
 
     const hello = (a) => {
         alert(a + '!!')
     }
 
-    let a = 1;
+    // 存
+    chrome.storage.sync.set({key: value}, function() {
+        console.log('Value is set to ' + value);
+    });
 
-    let b = 2;
+    // 取
+    chrome.storage.sync.get(['key'], function(result) {
+        console.log('Value currently is ' + result.key);
+    });
 
-    let c = 3;
-
-    let d = 4;
-
-    let e = 5;
 })()
