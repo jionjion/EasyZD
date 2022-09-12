d13ec6eaff1c32961e3175309e9ff8af1039e4bf
jionjion <jionjion@aliyun.com>
2022-09-12 21:28:16 +0800

    - 保存配置信息

--- src/js/options.js
+++ src/js/options.js
@@ -38,86 +38,163 @@ const DEFAULT_CONFIG = {
  */
 const CONFIG_LOCAL_STORE_KEY = "CUSTOMIZED_CONFIG";
 
+/**
+ * 刷新全局变量
+ */
+const refreshDom = () => {
+    $buttonSettingSave = document.querySelector("#setting-save-btn");
+    $textInputAppCodeKey = document.querySelector("#app_code_key");
+    $checkedInputEnableDrawTranslation = document.querySelector("#enable_draw_translation");
+    $radioInputDrawTranslationSecondaryKeyByNone = document.querySelector("#draw_translation_secondary_key_by_none");
+    $radioInputDrawTranslationSecondaryKeyByCtrl = document.querySelector("#draw_translation_secondary_key_by_ctrl");
+    $radioInputDrawTranslationSecondaryKeyByAlt = document.querySelector("#draw_translation_secondary_key_by_alt");
+    $radioInputDrawTranslationDefaultVoiceByUk = document.querySelector("#draw_translation_default_voice_by_uk");
+    $radioInputDrawTranslationDefaultVoiceByUs = document.querySelector("#draw_translation_default_voice_by_us");
+}
 
-// 页面加载后执行
-(() => {
-    /**
-     * 初始化值..
-     * @param {object} config 配置
-     */
-    const init = (config) => {
-        console.log(config)
-        debugger;
-        // 个人定制,不显示
-        if (Ext.isEmpty(config)) {
-            return;
-        }
+/**
+ * 保存配置
+ */
+const saveConfig = () => {
 
-        if (Ext.isNotEmpty(config.appCodeKey)) {
-            $textInputAppCodeKey.setAttribute("value", config.appCodeKey);
-        }
+    const config = configExtract();
 
-        if (Ext.isNotEmpty(config.enableDrawTranslation)) {
-            if (config.enableDrawTranslation === 'Y') {
-                $checkedInputEnableDrawTranslation.setAttribute("value", "checked");
-            } else {
-                $checkedInputEnableDrawTranslation.removeAttribute("checked");
-            }
-        }
+    // 保存到里面
+    chrome.storage.local.set({customizedConfig: config}, function () {
+        console.log('储存当前配置文件: ' + config);
+    });
 
-        if (Ext.isNotEmpty(config.drawTranslationSecondaryKey)) {
-            if (config.drawTranslationSecondaryKey === "None") {
-                $radioInputDrawTranslationSecondaryKeyByNone.setAttribute("checked", "checked");
-                $radioInputDrawTranslationSecondaryKeyByCtrl.removeAttribute("checked");
-                $radioInputDrawTranslationSecondaryKeyByAlt.removeAttribute("checked");
+    // 存
+    chrome.storage.local.set({key: config}, function () {
+        console.log('Value is set to ' + config);
+    });
 
-            } else if (config.drawTranslationSecondaryKey === "Ctrl") {
-                $radioInputDrawTranslationSecondaryKeyByCtrl.setAttribute("checked", "checked");
-                $radioInputDrawTranslationSecondaryKeyByNone.removeAttribute("checked");
-                $radioInputDrawTranslationSecondaryKeyByAlt.removeAttribute("checked");
+}
 
-            } else if (config.drawTranslationSecondaryKey === "Alt") {
-                $radioInputDrawTranslationSecondaryKeyByAlt.setAttribute("checked", "checked");
-                $radioInputDrawTranslationSecondaryKeyByNone.removeAttribute("checked");
-                $radioInputDrawTranslationSecondaryKeyByCtrl.removeAttribute("checked");
-            }
-        }
+/**
+ *  获取页面中的配置信息
+ *
+ * @return {string} 配置文件信息
+ */
+const configExtract = () => {
+    // 刷新全局变量
+    refreshDom();
 
-        if (Ext.isNotEmpty(config.drawTranslationDefaultVoice)) {
-            if (config.drawTranslationDefaultVoice === 'uk') {
-                $radioInputDrawTranslationDefaultVoiceByUk.setAttribute("checked", "checked");
-                $radioInputDrawTranslationDefaultVoiceByUs.removeAttribute("checked");
-            } else if (config.drawTranslationDefaultVoice === 'us') {
-                $radioInputDrawTranslationDefaultVoiceByUs.setAttribute("checked", "checked");
-                $radioInputDrawTranslationDefaultVoiceByUk.removeAttribute("checked");
-            }
-        }
+    let config = {};
+
+    let appCodeKey = $textInputAppCodeKey.getAttribute("value");
+
+    if (Ext.isNotEmpty(appCodeKey)) {
+        config['appCodeKey'] = appCodeKey;
+    }
+
+    // 是否启用划词
+    let enableDrawTranslation = $checkedInputEnableDrawTranslation.hasAttribute("checked") || ($checkedInputEnableDrawTranslation['checked'] !== false) ? 'Y' : 'N';
+    if (Ext.isNotEmpty(enableDrawTranslation)) {
+        config['enableDrawTranslation'] = enableDrawTranslation;
+    }
+
+    // 划词的配合键
+    if ($radioInputDrawTranslationSecondaryKeyByNone.hasAttribute("checked") || $radioInputDrawTranslationSecondaryKeyByNone['checked']) {
+        config['drawTranslationSecondaryKey'] = 'None';
+    } else if ($radioInputDrawTranslationSecondaryKeyByCtrl.hasAttribute("checked") || $radioInputDrawTranslationSecondaryKeyByCtrl['checked']) {
+        config['drawTranslationSecondaryKey'] = 'Ctrl';
+    } else if ($radioInputDrawTranslationSecondaryKeyByAlt.hasAttribute("checked") || $radioInputDrawTranslationSecondaryKeyByAlt['checked']) {
+        config['drawTranslationSecondaryKey'] = 'Alt';
+    } else {
+        config['drawTranslationSecondaryKey'] = 'None';
+    }
+
+    // 发音
+    if($radioInputDrawTranslationDefaultVoiceByUk.hasAttribute("checked") || $radioInputDrawTranslationDefaultVoiceByUk['checked']){
+        config['drawTranslationDefaultVoice'] = 'uk';
+    }else if($radioInputDrawTranslationDefaultVoiceByUs.hasAttribute("checked") || $radioInputDrawTranslationDefaultVoiceByUs['checked']){
+        config['drawTranslationDefaultVoice'] = 'us';
     }
 
 
-    // 初始化值
-    init(DEFAULT_CONFIG);
+    console.log(config);
 
+    return JSON.stringify(config);
+}
+
+/**
+ *  初始化配置文件
+ *
+ * @param config
+ */
+const configApply = (config) => {
+
+    // 个人定制,不显示
+    if (Ext.isEmpty(config)) {
+        return;
+    }
+
+    if (Ext.isNotEmpty(config.appCodeKey)) {
+        $textInputAppCodeKey.setAttribute("value", config.appCodeKey);
+    }
+
+    if (Ext.isNotEmpty(config.enableDrawTranslation)) {
+        if (config.enableDrawTranslation === 'Y') {
+            $checkedInputEnableDrawTranslation['checked'] = true
+        } else {
+            $checkedInputEnableDrawTranslation.removeAttribute("checked");
+        }
+    }
+
+    if (Ext.isNotEmpty(config.drawTranslationSecondaryKey)) {
+        if (config.drawTranslationSecondaryKey === "None") {
+            $radioInputDrawTranslationSecondaryKeyByNone['checked'] = true
+            $radioInputDrawTranslationSecondaryKeyByCtrl.removeAttribute("checked");
+            $radioInputDrawTranslationSecondaryKeyByAlt.removeAttribute("checked");
+
+        } else if (config.drawTranslationSecondaryKey === "Ctrl") {
+            $radioInputDrawTranslationSecondaryKeyByCtrl['checked'] = true
+            $radioInputDrawTranslationSecondaryKeyByNone.removeAttribute("checked");
+            $radioInputDrawTranslationSecondaryKeyByAlt.removeAttribute("checked");
+
+        } else if (config.drawTranslationSecondaryKey === "Alt") {
+            $radioInputDrawTranslationSecondaryKeyByAlt['checked'] = true
+            $radioInputDrawTranslationSecondaryKeyByNone.removeAttribute("checked");
+            $radioInputDrawTranslationSecondaryKeyByCtrl.removeAttribute("checked");
+        }
+    }
+
+    if (Ext.isNotEmpty(config.drawTranslationDefaultVoice)) {
+        if (config.drawTranslationDefaultVoice === 'uk') {
+            $radioInputDrawTranslationDefaultVoiceByUk['checked'] = true
+            $radioInputDrawTranslationDefaultVoiceByUs.removeAttribute("checked");
+        } else if (config.drawTranslationDefaultVoice === 'us') {
+            $radioInputDrawTranslationDefaultVoiceByUs['checked'] = true
+            $radioInputDrawTranslationDefaultVoiceByUk.removeAttribute("checked");
+        }
+    }
+}
+
+
+// 页面加载后执行
+(() => {
+
+    // 取
+    chrome.storage.local.get(['key'], function (result) {
+        console.log('Value currently is ' + result.key);
+    });
 
     // 绑定事件, 保存按钮事件
     $buttonSettingSave.addEventListener("click", saveConfig);
 
+    // 获取配置文件. 如果没有给默认的配置文件
+    chrome.storage.local.get(['customizedConfig'], function (result) {
+        const config = result.customizedConfig;
+        console.log("获取当前配置文件: " + config)
 
-    function saveConfig() {
-
-        var number = Math.random() * 10000;
-
-
-        // 保存到里面
-        chrome.storage.local.set({"key": number}, function () {
-            console.log('Value is set to ' + number);
-        });
-
-        // 取
-        chrome.storage.local.get(["key"], function (result) {
-            console.log('Value currently is ' + result.key);
-        });
-    }
-
+        if (Ext.isNotEmpty(config)) {
+            // 初始化配置文件
+            configApply(JSON.parse(config));
+        } else {
+            // 初始化默认配置文件
+            configApply(DEFAULT_CONFIG);
+        }
+    });
 })()
 
